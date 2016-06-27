f4e60e53246c83905d888ca3f7a6aec4cf2f0104
ranguisheng <guisheng.ran@gmail.com>
2016-06-27 17:02:37 +0800

    delete minggan

--- test/loginJd.py
+++ test/loginJd.py
@@ -6,10 +6,10 @@ import traceback
 import base64
 
 RETRY_COUNT=5
-loginPageUrl = "https://passportRes.jd.com/uc/login"
-loginPostUrl = "http://passportRes.jd.com/uc/loginService"
-userName='MTM0MjYzMTQ2NTM='
-password='MjA5MDM4cmdz'
+loginPageUrl = "https://passport.jd.com/new/login.aspx"
+loginPostUrl = "http://passport.jd.com/uc/loginService"
+userName=''
+password=''
 #定义连接函数，有超时重连功能
 def Navigate(url,data={}):           
     tryTimes = 0
@@ -18,10 +18,11 @@ def Navigate(url,data={}):
             print("尝试%s次之后仍无法链接网络，程序终止" % RETRY_COUNT)
             break
         try:
+            print('data type: %s' % type(data))
             if (data=={}):
                 req = urllib.request.Request(url)
             else:
-                req = urllib.request.Request(url,urllib.parse.urlencode(data))
+                req = urllib.request.Request(url,urllib.parse.urlencode(data).encode('utf-8'))
             req = urllib.request.urlopen(req).read()
             tryTimes = tryTimes +1
         except socket.error:
@@ -49,12 +50,14 @@ def packagePostData():
     loginSoup = BeautifulSoup(login,'html.parser')
     #查找登陆参数中的uuid
     uuid = loginSoup.find_all("form")[0].find_all("input")[0]['value']
-    #print uuid
+    print('uuid: %s' % uuid)
      
     #查找登陆参数中的随机值，class为clr
-    clr = loginSoup.find_all("span","clr")[0]
-    clrName = clr.find_next_siblings("input")[0]['name']  
-    clrValue = clr.find_next_siblings("input")[0]['value']
+#     clr = loginSoup.find_all("span","clr")[0]
+#     clrName = clr.find_next_siblings("input")[0]['name']  
+#     clrValue = clr.find_next_siblings("input")[0]['value']
+    clrName = 'aaa'  
+    clrValue = 'bbb'
     #print clrName,clrValue
      
     #print url
@@ -69,14 +72,14 @@ def packagePostData():
       'uuid':uuid,
       'authcode':''
     }
+    print('postData')
+    print(postData)
     return postData
 if __name__=='__main__':
-#     processCookie()
-#     passportRes = Navigate(loginPostUrl,packagePostData())
-#     print(passportRes)
-    req = urllib.request.Request(loginPageUrl)
-    req1 = urllib.request.urlopen(req).read()
-    print(req)
+    processCookie()
+    passportRes = Navigate(loginPostUrl,packagePostData())
+    print('login response: %s' % passportRes)
+    #\\u8bf7\\u5237\\u65b0\\u9875\\u9762\\u540e\\u91cd\\u65b0\\u63d0\\u4ea4   请刷新页面后重新提交
      
       
     
